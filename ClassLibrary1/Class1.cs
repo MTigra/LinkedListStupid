e2fbf6f737f3b5a5833eb8d2013bffc20282ffc6
Тигран Мартиросян <tomartirosyan@edu.hse.ru>
2018-03-13 19:29:15 +0300

    kek

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class CircularLinkedList<T>
+    {
+        
+            List<LinkedListNode<T>> list = new List<LinkedListNode<T>>();
+        private int curindx = -1;
+
+        public LinkedListNode<T> Current
+        {
+            get;
+            private set;
+        }
+
+        public CircularLinkedList(List<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                this.list.Add(new LinkedListNode<T>(list[i]));
+            }
+        }
+
+        public CircularLinkedList()
+        {
+
+        }
+
+        public void Add(T el)
+        {
+            list.Add(new LinkedListNode<T>(el));
+
+        }
+
+
+        public void Shift()
+        {
+            if (++curindx >= list.Count)
+            {
+                curindx = 0;
+            }
+            if (list.Count <= 0) Current = default(LinkedListNode<T>);
+            else
+            { Current = list[curindx]; }
+
+
+        }
+
+        public T Remove()
+        {
+            if(Current == null) throw new IndexOutOfRangeException("список пуст");
+            T temp = Current.Value;
+            list.Remove(Current);
+            if (curindx >= list.Count)
+            {
+                Shift();
+            }
+            else
+            {
+
+                Current = list[curindx];
+            }
+            return temp;
+        }
+    }
+}
---
+++ ConsoleApplication27/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                       
+                List<int> lst = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+                CircularLinkedList<int> circular = new CircularLinkedList<int>(lst);
+                do
+                {
+                    circular.Shift();
+                    Console.WriteLine(circular.Current.Value);
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+                Console.WriteLine("Удаления");
+                do
+                {
+                    Console.WriteLine("удалил: " + circular.Remove());
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+                circular.Shift();
+                var a = circular.Current;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
